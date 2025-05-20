ec293eb365485ea690f77867e799d8491027126e
Martijn Govers <Martijn.Govers@Alliander.com>
2025-05-20 13:54:37 +0200

    minor.
    Signed-off-by: Martijn Govers <Martijn.Govers@Alliander.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,4 @@ git clone git@github.com:PowerGridModel/power-grid-model.git
 cd power-grid-model
 git status
 
-./build.sh -p gcc_debug -e -i -t || exit 1
+sh ./build.sh -p gcc_debug -e -i -t || exit 1
